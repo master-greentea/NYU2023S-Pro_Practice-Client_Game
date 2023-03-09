e6730cfd3f3c49417114135b911c8c70e76171d3
Steve Hu <yh3133@nyu.edu>
2023-03-09 01:39:10 -0500

    minigame scene complete

---
+++ Assets/Minigame/MinigameDrag.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MinigameDrag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    public bool isGoodItem;
+    private Image _image;
+    private Vector3 _startPos;
+
+    void Start()
+    {
+        _image = GetComponent<Image>();
+        _startPos = transform.localPosition;
+    }
+
+    private void Update()
+    {
+        if (MinigameManager.Instance.isGameEnded)
+        {
+            _image.color = Color.gray;
+            transform.localPosition = _startPos;
+            return;
+        }
+        _image.color = isGoodItem ? Color.green : Color.white;
+    }
+
+    public void OnBeginDrag(PointerEventData ctx)
+    {
+        if (MinigameManager.Instance.isGameEnded) return;
+        _image.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData ctx)
+    {
+        if (MinigameManager.Instance.isGameEnded) return;
+        transform.position = ctx.position;
+    }
+
+    public void OnEndDrag(PointerEventData ctx)
+    {
+        if (MinigameManager.Instance.isGameEnded) return;
+        transform.localPosition = _startPos;
+        _image.raycastTarget = true;
+    }
+}
